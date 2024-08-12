0d3d07f91e5dc3718963d8a2b9fdaa995dd8f73f
MalenaFG <malena.f.giusti@gmail.com>
2024-08-12 16:21:26 +0200

    cohorts route done

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cohortsData = require("./cohorts.json")
 
 const app = express();
 
@@ -11,6 +12,10 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+app.get('/api/cohorts', (req, res) => {
+  res.json(cohortsData)
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
