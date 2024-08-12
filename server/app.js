392986b119732654d82a9c779b74f803e7a9215a
Ruben Diaz <rubendb95@gmail.com>
2024-08-12 16:45:15 +0200

    update Cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
 const PORT = 5005;
 const students = require('./students.json')
 const cohortsData = require("./cohorts.json")
@@ -12,12 +13,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({
+  origin: ['http://localhost:5173']
+}))
 
 app.get('/api/students', (req, res) => {
   res.json(students)
-
 })
-
 
 app.get('/api/cohorts', (req, res) => {
   res.json(cohortsData)
