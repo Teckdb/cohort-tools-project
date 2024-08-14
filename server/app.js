b8cbe29bd3a4b5208e8c2100c8aba549e0bac6c0
MalenaFG <malena.f.giusti@gmail.com>
2024-08-14 15:19:58 +0200

    modified:   server/app.js

--- server/app.js
+++ server/app.js
@@ -1,35 +1,33 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require('cors')
-const PORT = 5005;
+const express = require("express")
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+
+const PORT = 5005
 
 const mongoose = require("mongoose")
 
 const databaseName = 'cohort-tools-api'
 
-const MONGO_URI = `mongodb://127.0.0.1:27017/${databaseName}`;
+const MONGO_URI = `mongodb://127.0.0.1:27017/${databaseName}`
 
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
-    const dbName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${dbName}"`);
+    const dbName = x.connections[0].name
+    console.log(`Connected to Mongo! Database name: "${dbName}"`)
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+    console.error("Error connecting to mongo: ", err)
+  })
 
-const app = express();
+const app = express()
 
 
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors({
-  origin: ['http://localhost:5173']
-}))
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+a
 
 app.get('/api/students', (req, res) => {
   res.json(students)
@@ -40,5 +38,5 @@ app.get('/api/cohorts', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
